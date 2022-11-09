af56330416ec8409121097baa570491ae5135ddf
yunseo <cysdl5604@inu.ac.kr>
2022-11-09 16:08:26 +0900

    #3 model 작성 및 member 토큰 발행 로직 작성

---
+++ monster-factory-server/src/main/java/com/profeel/monsterfac/monsterfactoryserver/member/command/domain/model/Password.java
@@ -0,0 +1,41 @@
+package com.profeel.monsterfac.monsterfactoryserver.member.command.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+/**
+ * <pre>
+ * Class : Password
+ * Comment: 클래스에 대한 간단 설명
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2022-11-02       최은진           최초 생성
+ * </pre>
+ *
+ * @author 최은진(최초 작성자)
+ * @version 1(클래스 버전)
+ */
+
+@Embeddable
+public class Password {
+
+    @Column(name="member_pwd")
+    private String value;
+
+    protected Password() {}
+
+    public Password(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public boolean match(String password) {
+        return this.value.equals(password);
+    }
+}
+
