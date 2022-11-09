9167f0b89871655c042a966d4e13583d807e4516
yunseo <cysdl5604@inu.ac.kr>
2022-11-09 16:33:28 +0900

    #3 도메인 서비스, config 파일 작성

--- monster-factory-server/src/main/java/com/profeel/monsterfac/monsterfactoryserver/member/command/application/service/ReqMemberService.java
+++ monster-factory-server/src/main/java/com/profeel/monsterfac/monsterfactoryserver/member/command/application/service/ReqMemberService.java
@@ -11,8 +11,8 @@ import com.profeel.monsterfac.monsterfactoryserver.member.command.domain.service
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.Date;
 
 @Service
---
+++ monster-factory-server/src/main/java/com/profeel/monsterfac/monsterfactoryserver/member/command/infra/service/MemberServiceImpl.java
@@ -0,0 +1,25 @@
+package com.profeel.monsterfac.monsterfactoryserver.member.command.infra.service;
+
+
+import com.profeel.monsterfac.monsterfactoryserver.common.annotation.DomainService;
+import com.profeel.monsterfac.monsterfactoryserver.member.command.domain.service.MemberService;
+
+/**
+ * <pre>
+ * Class : MemberServiceImpl
+ * Comment: 클래스에 대한 간단 설명
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2022-11-04       최은진           최초 생성
+ * </pre>
+ *
+ * @author 최은진(최초 작성자)
+ * @version 1(클래스 버전)
+ */
+
+@DomainService
+public class MemberServiceImpl implements MemberService {
+
+}
