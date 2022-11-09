989f219242e72617331d6b10dca1abe64cc33000
yunseo <cysdl5604@inu.ac.kr>
2022-11-09 15:08:44 +0900

    #1 전체적인 파일 구조 세팅

---
+++ monster-factory-server/src/test/java/com/profeel/monsterfac/monsterfactoryserver/MonsterFactoryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profeel.monsterfac.monsterfactoryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonsterFactoryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
