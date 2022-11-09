385091bd5f46b61028b8491e4edc8d2cdcfe27cf
yunseo <cysdl5604@inu.ac.kr>
2022-11-09 15:52:52 +0900

    #1 jwt token 로직 작성

--- monster-factory-server/src/main/java/com/profeel/monsterfac/monsterfactoryserver/MonsterFactoryServerApplication.java
+++ monster-factory-server/src/main/java/com/profeel/monsterfac/monsterfactoryserver/MonsterFactoryServerApplication.java
@@ -2,12 +2,18 @@ package com.profeel.monsterfac.monsterfactoryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class MonsterFactoryServerApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml, "
+			+ "classpath:aws.yml";
 	public static void main(String[] args) {
-		SpringApplication.run(MonsterFactoryServerApplication.class, args);
+		new SpringApplicationBuilder(MonsterFactoryServerApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+						.run(args);
 	}
 
 }
---
+++ monster-factory-server/src/main/java/com/profeel/monsterfac/monsterfactoryserver/jwt/exception/TokenException.java
@@ -0,0 +1,35 @@
+package com.profeel.monsterfac.monsterfactoryserver.jwt.exception;
+
+/**
+ * <pre>
+ * Class : TokenException
+ * Comment: 클래스에 대한 간단 설명
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2022-11-03       최은진           최초 생성
+ * </pre>
+ *
+ * @author 최은진(최초 작성자)
+ * @version 1(클래스 버전)
+ */
+
+public class TokenException extends RuntimeException {
+
+    public TokenException() {
+        super();
+    }
+
+    public TokenException(String message) {
+        super(message);
+    }
+
+    public TokenException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TokenException(Throwable cause) {
+        super(cause);
+    }
+}
