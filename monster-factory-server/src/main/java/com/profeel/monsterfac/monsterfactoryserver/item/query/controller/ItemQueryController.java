b4f6dead7df87bce30a1f997412818f2d5a69788
carrtt1 <cej303030@gmail.com>
2022-11-11 11:06:34 +0900

    #34 아이템 전체 목록 조회 api 제작

---
+++ monster-factory-server/src/main/java/com/profeel/monsterfac/monsterfactoryserver/item/query/controller/ItemQueryController.java
@@ -0,0 +1,48 @@
+package com.profeel.monsterfac.monsterfactoryserver.item.query.controller;
+
+import com.profeel.monsterfac.monsterfactoryserver.item.query.data.ItemData;
+import com.profeel.monsterfac.monsterfactoryserver.item.query.service.ItemQueryService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * <pre>
+ * Class : ItemQueryController
+ * Comment: 클래스에 대한 간단 설명
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2022-11-11       최은진           최초 생성
+ * </pre>
+ *
+ * @author 최은진(최초 작성자)
+ * @version 1(클래스 버전)
+ */
+
+@RestController
+@RequestMapping("/items")
+public class ItemQueryController {
+
+    private ItemQueryService itemQueryService;
+
+    @Autowired
+    public ItemQueryController(ItemQueryService itemQueryService) {
+        this.itemQueryService = itemQueryService;
+    }
+
+    @ApiOperation(value = "아이템 목록 조회", notes = "아이템 전체 목록이 나오는 api이다")
+    @GetMapping
+    public ResponseEntity<?> findItem() {
+
+        List<ItemData> itemDataList = itemQueryService.findItem();
+
+        return ResponseEntity.ok().body(itemDataList);
+    }
+}
---
+++ monster-factory-server/src/main/java/com/profeel/monsterfac/monsterfactoryserver/item/query/service/ItemQueryService.java
@@ -0,0 +1,40 @@
+package com.profeel.monsterfac.monsterfactoryserver.item.query.service;
+
+import com.profeel.monsterfac.monsterfactoryserver.item.query.data.ItemData;
+import com.profeel.monsterfac.monsterfactoryserver.item.query.repository.ItemQueryMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * <pre>
+ * Class : ItemQueryService
+ * Comment: 클래스에 대한 간단 설명
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2022-11-11       최은진           최초 생성
+ * </pre>
+ *
+ * @author 최은진(최초 작성자)
+ * @version 1(클래스 버전)
+ */
+
+@Service
+public class ItemQueryService {
+
+    private ItemQueryMapper itemQueryMapper;
+
+    @Autowired
+    public ItemQueryService(ItemQueryMapper itemQueryMapper) {
+        this.itemQueryMapper = itemQueryMapper;
+    }
+
+    public List<ItemData> findItem() {
+
+        return itemQueryMapper.findItem();
+    }
+}
