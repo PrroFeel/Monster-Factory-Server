367933d890a9c600f2b4205f1b94803fe8b80a6f
carrtt1 <cej303030@gmail.com>
2022-11-12 18:04:53 +0900

    #43 gameId model 제작

---
+++ monster-factory-server/src/main/java/com/profeel/monsterfac/monsterfactoryserver/game/command/domain/model/GameId.java
@@ -0,0 +1,15 @@
+package com.profeel.monsterfac.monsterfactoryserver.game.command.domain.model;/**
+ * <pre>
+ * Class : GameId
+ * Comment: 클래스에 대한 간단 설명
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2022-11-12       최은진           최초 생성
+ * </pre>
+ * @version 1(클래스 버전)
+ * @author 최은진(최초 작성자)
+ * @see (참고할 class 또는 외부 url)
+ */public class GameId {
+}
