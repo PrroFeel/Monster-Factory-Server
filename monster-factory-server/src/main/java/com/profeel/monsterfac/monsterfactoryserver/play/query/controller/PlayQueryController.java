9ce93bdd8f36a8536307f8e77fa835332cb86df2
carrtt1 <cej303030@gmail.com>
2022-11-13 14:11:08 +0900

    #49 플레이 랭킹 컨트롤러 작성

--- monster-factory-server/src/main/java/com/profeel/monsterfac/monsterfactoryserver/play/query/controller/PlayQueryController.java
+++ monster-factory-server/src/main/java/com/profeel/monsterfac/monsterfactoryserver/play/query/controller/PlayQueryController.java
@@ -1,5 +1,17 @@
 package com.profeel.monsterfac.monsterfactoryserver.play.query.controller;
 
+import com.profeel.monsterfac.monsterfactoryserver.play.query.data.PlayRankData;
+import com.profeel.monsterfac.monsterfactoryserver.play.query.service.PlayQueryService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /**
  * <pre>
  * Class : PlayQueryController
@@ -15,5 +27,23 @@ package com.profeel.monsterfac.monsterfactoryserver.play.query.controller;
  * @version 1(클래스 버전)
  */
 
+@RestController
+@RequestMapping("/play")
 public class PlayQueryController {
+
+    private PlayQueryService playQueryService;
+
+    @Autowired
+    public PlayQueryController(PlayQueryService playQueryService) {
+        this.playQueryService = playQueryService;
+    }
+
+    @ApiOperation(value = "플레이 랭킹 조회", notes = "플레이 랭킹이 나오는 api이다")
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findRankingByGameId(@PathVariable("id") int playGameId) {
+
+        List<PlayRankData> GameRanking = playQueryService.findRankingByGameId(playGameId);
+
+        return ResponseEntity.ok().body(GameRanking);
+    }
 }
