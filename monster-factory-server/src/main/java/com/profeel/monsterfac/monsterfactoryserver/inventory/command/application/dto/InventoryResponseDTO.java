3072d90c81d60c96b2960deddfda1ddbdf475663
carrtt1 <cej303030@gmail.com>
2022-11-13 21:22:35 +0900

    #56 인벤토리 regist 결과 DTO 작성

---
+++ monster-factory-server/src/main/java/com/profeel/monsterfac/monsterfactoryserver/inventory/command/application/dto/InventoryResponseDTO.java
@@ -0,0 +1,48 @@
+package com.profeel.monsterfac.monsterfactoryserver.inventory.command.application.dto;
+
+import com.profeel.monsterfac.monsterfactoryserver.inventory.command.domain.model.HoldingItems;
+import com.profeel.monsterfac.monsterfactoryserver.inventory.command.domain.model.InventoryOwner;
+
+/**
+ * <pre>
+ * Class : InventoryResponseDTO
+ * Comment: 클래스에 대한 간단 설명
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2022-11-13       최은진           최초 생성
+ * </pre>
+ *
+ * @author 최은진(최초 작성자)
+ * @version 1(클래스 버전)
+ */
+
+public class InventoryResponseDTO {
+
+    private InventoryOwner ownerId;
+    private HoldingItems itemId;
+
+    public InventoryResponseDTO() {}
+
+    public InventoryResponseDTO(InventoryOwner ownerId, HoldingItems itemId) {
+        this.ownerId = ownerId;
+        this.itemId = itemId;
+    }
+
+    public InventoryOwner getOwnerId() {
+        return ownerId;
+    }
+
+    public HoldingItems getItemId() {
+        return itemId;
+    }
+
+    @Override
+    public String toString() {
+        return "InventoryResponseDTO{" +
+                "ownerId='" + ownerId + '\'' +
+                ", itemId=" + itemId +
+                '}';
+    }
+}
