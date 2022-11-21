3a0de9c2e96ee287a26fa6c3c7525c10b330889e
yunseo <cysdl5604@inu.ac.kr>
2022-11-21 15:25:26 +0900

    #86 Multipart -> File 변환 관련 로직 주석 처리

--- monster-factory-server/src/main/java/com/profeel/monsterfac/monsterfactoryserver/file/command/domain/service/FileService.java
+++ monster-factory-server/src/main/java/com/profeel/monsterfac/monsterfactoryserver/file/command/domain/service/FileService.java
@@ -7,10 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.annotation.PostConstruct;
-import java.io.File;
-import java.io.IOException;
-import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -35,28 +31,27 @@ public class FileService {
     @Value("images")
     private String imageDir;
 
-    // 현재 프로파일 ( local / development )
-    @Value("${spring.environment}")
-    private String environment;
+//    // 현재 프로파일 ( local / development )
+//    @Value("${spring.environment}")
+//    private String environment;
+//
+//    // 파일 임시 저장 경로
+//    @Value("${spring.file-dir}")
+//    private String fileDir;
+//    private String filePath;
 
-    // 파일 임시 저장 경로
-    @Value("${spring.file-dir}")
-    private String fileDir;
-    private String filePath;
-
-    @PostConstruct
-    private void init(){
-        // 현재 프로파일에 맞게 임시 파일 경로 설정
-        if(environment.equals("local")){
-            String fileFolder = System.getProperty("user.dir") + this.fileDir;
-            mkdirResource(fileFolder);
-            this.filePath = fileFolder;
-        } else if(environment.equals("development")){
-            String fileFolder =  "/var/www/html/files";
-            mkdirResource(fileFolder);
-            this.filePath = this.fileDir;
-        }
-    }
+//    @PostConstruct
+//    private void init(){
+////        System.out.println("현재 env(프로파일) : " + environment);
+////
+////        // 현재 프로파일에 맞게 임시 파일 경로 설정
+////        if(environment.equals("local")){
+////            String fileFolder = System.getProperty("user.dir") + this.fileDir;
+////            this.filePath = fileFolder;
+////        } else if(environment.equals("development")){
+////            this.filePath = this.fileDir;
+////        }
+//    }
     public String createSavePath(String dirName, String saveFileName){
         return String.format("%s/%s", dirName, saveFileName);
     }
@@ -96,13 +91,13 @@ public class FileService {
 //        );
 //    }
 
-    private static void mkdirResource(String fileDir){
-        File Folder = new File(fileDir);
-
-        if(!Folder.exists()) {
-            Folder.mkdir(); // 폴더 생성
-        }
-    }
+//    private static void mkdirResource(String fileDir){
+//        File Folder = new File(fileDir);
+//
+//        if(!Folder.exists()) {
+//            Folder.mkdir(); // 폴더 생성
+//        }
+//    }
 
     // UUID로 파일명을 변경하는 메소드
     public String createSaveFilename(String ext){
@@ -117,24 +112,25 @@ public class FileService {
         return originalFilename.substring(pos+1);
     }
 
-    public File convertToFile(MultipartFile multipartFile) throws IOException {
-        return convert(multipartFile)
-                .orElseThrow(()-> new IllegalArgumentException("error: MultipartFile -> File convert fail"));
-    }
-    // Multipart 파일을 임시 저장 경로의 File 객채로 변환 메소드
-    private Optional<File> convert(MultipartFile multipartFile) throws IOException {
-        if(multipartFile.isEmpty()){
-            return Optional.empty();
-        }
-
-        String ext = extractExt(multipartFile.getOriginalFilename());
-        String changedFilename = createSaveFilename(ext);
-
-        File file = new File(filePath+changedFilename);
-        multipartFile.transferTo(file);
-
-        return Optional.of(file);
-    }
+//    public File convertToFile(MultipartFile multipartFile) throws IOException {
+//        return convert(multipartFile)
+//                .orElseThrow(()-> new IllegalArgumentException("error: MultipartFile -> File convert fail"));
+//    }
+//    // Multipart 파일을 임시 저장 경로의 File 객채로 변환 메소드
+//    private Optional<File> convert(MultipartFile multipartFile) throws IOException {
+//        if(multipartFile.isEmpty()){
+//            return Optional.empty();
+//        }
+//
+//        String ext = extractExt(multipartFile.getOriginalFilename());
+//        String changedFilename = createSaveFilename(ext);
+//
+//        System.out.println("현재 filePath : " + filePath);
+//        File file = new File(filePath+changedFilename);
+//        multipartFile.transferTo(file);
+//
+//        return Optional.of(file);
+//    }
 
 
 
