f1b18b44f118d77a7f3323ef97f25a7efae704c9
yunseo <cysdl5604@inu.ac.kr>
2022-11-21 20:00:17 +0900

    #88 파일 정보 조회 서비스, 레파지토리 추가

--- monster-factory-server/src/main/java/com/profeel/monsterfac/monsterfactoryserver/file/command/domain/repository/FileInfoRepository.java
+++ monster-factory-server/src/main/java/com/profeel/monsterfac/monsterfactoryserver/file/command/domain/repository/FileInfoRepository.java
@@ -1,21 +1,22 @@
 package com.profeel.monsterfac.monsterfactoryserver.file.command.domain.repository;
 
-import com.profeel.monsterfac.monsterfactoryserver.file.command.domain.model.ModelingFileInfo;
+import com.profeel.monsterfac.monsterfactoryserver.file.command.domain.model.FileInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * <pre>
- * Class : projectRepository
+ * Class : FileInfoRepository
  * Comment: 클래스에 대한 간단 설명
  * History
  * ================================================================
  * DATE             AUTHOR           NOTE
  * ----------------------------------------------------------------
- * 2022-11-06       최윤서           최초 생성
+ * 2022-11-21       최윤서           최초 생성
  * </pre>
  *
  * @author 최윤서
  * @version 1
+ * @see 참고할 class 또는 외부 url
  */
-public interface ModelingFileRepository extends JpaRepository<ModelingFileInfo, Integer> {
+public interface FileInfoRepository extends JpaRepository<FileInfo, Integer> {
 }
